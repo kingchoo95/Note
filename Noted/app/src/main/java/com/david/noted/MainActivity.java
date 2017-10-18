fd795c596f6a1b59070b2468c5f934d6701ca7f7
David Choo <davidchoo09@gmail.com>
2017-10-18 15:29:52 +0800

    added title show on list view, long press delete, alert dialog for delete, add new notes's title ,place autocomplete

--- Noted/app/src/main/java/com/david/noted/MainActivity.java
+++ Noted/app/src/main/java/com/david/noted/MainActivity.java
@@ -1,13 +1,17 @@
 package com.david.noted;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,8 +20,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    ArrayList<String> titles = new ArrayList<>();
+    //private TextView mTextMessage;
+    static ArrayList<String> titles = new ArrayList<>();
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(MenuItem item) {
             if(item.getItemId()== R.id.addNewNoteId) {
 
-                Log.i("Add Button", "buttoon click");
+                //Log.i("Add Button", "buttoon click");
                 Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
 
                // intent.putExtra("noteId", );
@@ -37,27 +43,58 @@ public class MainActivity extends AppCompatActivity {
                 return false;
 
             }
-
         }
 
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        titles.add("Do more exercise");
         ListView listView = (ListView) findViewById(R.id.notesListViewId);
 
         //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.addnote_menu);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,titles);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,titles);
 
         listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+                //tell which row the user tap
+                intent.putExtra("noteId",position);
 
+                startActivity(intent);
+            }
+        });
+
+        //long click to delete note
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view,final  int position, long id) {
+                final int itemToDelete = position;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                titles.remove(itemToDelete);
+                                MainActivity.arrayAdapter.notifyDataSetChanged();
+                            }
+                        }).setNegativeButton("No",null).show();
+                return true;
+            }
+        });
 
     }
 
--- Noted/app/src/main/java/com/david/noted/NoteEditorActivity.java
+++ Noted/app/src/main/java/com/david/noted/NoteEditorActivity.java
@@ -1,16 +1,24 @@
 package com.david.noted;
 
 import android.app.Dialog;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class NoteEditorActivity extends AppCompatActivity {
 
@@ -18,6 +26,7 @@ public class NoteEditorActivity extends AppCompatActivity {
     String dates[]={"Today","Tomorrow","Pick a date"};
     String times[]={"Select a Time"};
     String repeats[]={"Does not repeat","Daily","Weekly","Monthly","Yearly"};
+    int noteId;
 
     //defines array adapter of string type
     ArrayAdapter dateAdapter,timeAdapter,repeatAdapter;
@@ -31,6 +40,44 @@ public class NoteEditorActivity extends AppCompatActivity {
         dateAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, dates);
         timeAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, times);
         repeatAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, repeats);
+
+        EditText editTextTitle = (EditText) findViewById(R.id.editTextTitleId);
+        EditText editTextNotes = (EditText) findViewById(R.id.editTextAddNoteHereId);
+
+        Intent intent = getIntent();
+
+        noteId = intent.getIntExtra("noteId",-1);
+
+        if(noteId != -1 ){
+
+            editTextTitle.setText(MainActivity.titles.get(noteId));
+            //editTextNotes.setText(MainActivity.notes.get(noteId));
+        }else{
+            MainActivity.titles.add("");
+           // MainActivity.notes.add("");
+            noteId = MainActivity.titles.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
+
+        editTextTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                MainActivity.titles.set(noteId,String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
     }
     // menu bar for add reminder start
@@ -63,6 +110,24 @@ public class NoteEditorActivity extends AppCompatActivity {
             repeatSp.setAdapter(repeatAdapter);
             dateSp.setAdapter(dateAdapter);
             timeSp.setAdapter(timeAdapter);
+
+            PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                    getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+            //auto complete
+            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    // TODO: Get info about the selected place.
+                    Log.i("tag", "Place: " + place.getName());
+                }
+
+                @Override
+                public void onError(Status status) {
+                    // TODO: Handle the error.
+                    Log.i("tag", "An error occurred: " + status);
+                }
+            });
 
             dialog.show();
             return true;
